c2f017ee8017425fe1971484be7cdc140bf0baee
hanjeong-u <jwooh0208@gmail.com>
2025-05-12 23:52:34 +0900

    멀티 스레드 공부

---
+++ thread/control/interrupt/ThreadStopMainV3.java
@@ -0,0 +1,35 @@
+package thread.control.interrupt;
+
+import static java.lang.Thread.sleep;
+import static util.MyLogger.log;
+
+public class ThreadStopMainV3 {
+    public static void main(String[] args) throws InterruptedException {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+        sleep(100); // 시간을 줄임
+        log("작업 중단 지시 - thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) { // 인터럽트 상태 변경 log("작업 중");
+            }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+            try {
+                log("자원 정리 시도");
+                sleep(1000);
+                log("자원 정리 완료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " +
+                        Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
+        }
+    }
+}
